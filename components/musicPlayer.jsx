4486916f51202c138a6914019822ec980e299594
engelrolling <guillaume.engelroll@gmail.com>
2022-11-22 15:17:02 +0200

    chore(player): add music player for playlists

---
+++ components/musicPlayer.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react'
+
+export default function MusicPlayer({ tracks }) {
+    const [position, setPosition] = useState(0)
+    const [audio, setAudio] = useState(new Audio())
+
+    useEffect(() => {
+        if (tracks.length && position !== 0) {
+            blindTestLoad()
+            blindTestPlay()
+        }
+    }, [position])
+
+    const blindTestLoad = () => {
+        if (tracks.length) {
+            audio.src = tracks[position].preview_url
+            audio.load()
+        }
+    }
+
+    const blindTestPlay = () => {
+        if (!audio.paused || !audio.src || position === 0) {
+            blindTestLoad()
+        }
+        audio.play()
+        // audio.onended = () => { blindTestNext() }
+        console.log(`Playing ${tracks[position].name} by ${tracks[position].artists[0].name}`)
+    }
+
+    const blindTestNext = () => {
+        setPosition(position + 1)
+        console.log('Next song...')
+    }
+
+    const blindTestPause = () => {
+        audio.pause()
+        console.log("Paused")
+    }
+
+    return (
+        <div>
+            <button onClick={blindTestPlay}>Play</button>
+            <button onClick={blindTestPause}>Pause</button>
+            <button onClick={blindTestNext}>Next</button>
+        </div>
+    )
+}
